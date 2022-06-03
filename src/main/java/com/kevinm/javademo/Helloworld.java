a666616229b80a6d90c4bc618210a38e1225b395
Kevin Meng <kevin.meng@tlscontact.com>
2022-06-03 19:15:15 +0800

    get hostname

--- src/main/java/com/kevinm/javademo/Helloworld.java
+++ src/main/java/com/kevinm/javademo/Helloworld.java
@@ -5,16 +5,35 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+
 @RestController
 @EnableAutoConfiguration
 public class Helloworld {
 
-    @RequestMapping("/")
-    String sayHello() {
-        return "Hello World!";
-    }
+	@RequestMapping("/")
+	String sayHello() {
+		String hostName = System.getenv("HOSTNAME");
+		// get the os name
+		String os = System.getProperty("os.name");
+		System.out.println("hostname="+hostName);
+		System.out.println("os="+os);
 
-    public static void main(String[] args) {
-        SpringApplication.run(Helloworld.class, args);
-    }
+		// if the application is not running in a docker container, we can to obtain the
+		// hostname using the "java.net.InetAddress" class
+		if (hostName == null || hostName.isEmpty()) {
+			try {
+				InetAddress addr = InetAddress.getLocalHost();
+				hostName = addr.getHostName();
+			} catch (Exception e) {
+				System.err.println(e);
+				hostName = "Unknow";
+			}
+		}
+		return "Hello World!" + hostName;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Helloworld.class, args);
+	}
 }
